02b1816ac4749a057dd9131e4b52742ca6ccf9a7
QiHong Lu <lvqihong1992@gmail.com>
2019-06-08 14:21:49 -0400

    softmax demo

---
+++ softmax_temp.py
@@ -0,0 +1,28 @@
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+from scipy.special import softmax
+
+sns.set(style='white', context='talk', palette='colorblind')
+
+x = [.4, .3, .3]
+betas = [0, .1, .3, 1, 3, 10]
+
+# preproc
+n_xs = len(x)
+n_bs = len(betas)
+x = np.array(x)
+
+b_pal = sns.color_palette('RdBu', n_colors=n_bs)
+f, ax = plt.subplots(1, 1, figsize=(5, 5))
+
+for i, beta in enumerate(betas):
+    y = softmax(x/beta)
+    ax.plot(y, color=b_pal[i])
+
+ax.plot(x, color='k')
+f.legend([b for b in betas]+['raw'])
+
+# ax.set_ylim([0, 1])
+sns.despine()
+f.tight_layout()
